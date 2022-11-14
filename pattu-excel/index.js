34393272751a3b3c6f43042ebe0a1f794cd37814
bavareddy <117837230+bavareddy@users.noreply.github.com>
2022-11-14 07:19:46 +0530

    Add files via upload

---
+++ pattu-excel/index.js
@@ -0,0 +1,91 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const multer = require('multer')
+// const Student  = require('./studentSchema')
+const csvtojson = require('csvtojson')
+const fs=require('fs')
+const path=require('path')
+const app = express()
+
+mongoose.connect('mongodb://localhost:27017/MongoExcelDemo').then(() => {     // MongoDB connection
+    console.log('database connected')
+});
+
+
+app.use(express.static('public'))    // static folder
+app.set('view engine','ejs')             // set the template engine
+
+
+
+
+
+
+
+
+const studentSchema = mongoose.Schema({
+        name : {type : String, required : true},
+        email : {type: String, required : true},
+        img:{type: String, required : true}
+})
+
+
+Student= mongoose.model('Student', studentSchema)
+var excelStorage = multer.diskStorage({  
+    destination:(req,file,cb)=>{  
+         cb(null,__dirname+'/excelUploads');      // file added to the public folder of the root directory
+    },  
+    filename:(req,file,cb)=>{  
+         cb(null,file.originalname);  
+    }  
+});  
+var excelUploads = multer({storage:excelStorage}); 
+app.get('/',(req,res) => {
+       res.render('index.ejs');
+      
+       
+})
+// upload excel file and import in mongodb
+app.post('/uploadExcelFile', excelUploads.single("uploadfile"), (req, res) =>{  
+       importFile(__dirname+'/excelUploads/' + req.file.filename);
+            
+       function importFile(filePath){
+              //  Read Excel File to Json Data
+                var arrayToInsert = [];
+                csvtojson().fromFile(filePath).then(source => {
+              // Fetching the all data from each row
+              //obj={data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),contentType: 'image/png'}
+                for (var i = 0; i < source.length; i++) {
+                    console.log(source[i]["name"])
+                    var singleRow = {
+                        name: source[i]["name"],
+                        email: source[i]["email"],
+                        img: source[i] ["img"]
+                        
+                    };
+                    arrayToInsert.push(singleRow);
+                }
+             //inserting into the table student
+             Student.insertMany(arrayToInsert, (err, result) => {
+                    if (err) console.log(err);
+                        if(result){
+                            console.log("File imported successfully.");
+                            res.redirect('/')
+                        }
+                    });
+                });
+           }
+})
+
+
+
+app.listen(3000, () => {
+    console.log('server started at port 3000')
+})
+
+app.get('/post',(req,res)=>{
+    Student.find({},(err,data)=>{
+        res.render('post',{
+            data:data
+        })
+    })
+})
